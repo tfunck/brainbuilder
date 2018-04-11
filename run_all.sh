2ff504eaed97c71e28749d1c87e6eb22981d1eb9
tf <thomas.funck@mail.mcgill.ca>
2018-04-11 16:34:36 -0400

    added csv with slab 1 info

--- run_all.sh
+++ run_all.sh
@@ -1,11 +1,11 @@
 
 for mr in MR1 ; do
 	for h in R ; do 
-  		for slab in slab_1 ; do
-			#python3 detectLines.py  --epochs 5 --step 0.2 --train-source "test/" --train-output model_results/
+  		for slab in slab_1 slab_2 ; do
+			python3 detectLines.py  --epochs 5 --step 0.2 --train-source "test/" --train-output model_results/
 			
 			#Step 1 : Apply line removal
-			#python3 detectLines.py --step 0.2 --train-source "test/" --train-output model_results/  --raw-source raw/${h}_$slab --raw-output ${mr}/${h}_${slab}/lines_removed 
+			python3 detectLines.py --step 0.2 --train-source "test/" --train-output model_results/  --raw-source raw/${h}_$slab --raw-output ${mr}/${h}_${slab}/lines_removed 
 			
 			#Step 2: Automatic Cropping
 			echo python3 receptorCrop.py --source ${mr}/${h}_${slab}/lines_removed/final --output  ${mr}/${h}_${slab}/crop --step 0.2 --ext ".png"
