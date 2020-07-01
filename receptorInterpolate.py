e03d603932b4eb884d9042d80a25fbd7685046ad
tf <thomas.funck@mail.mcgill.ca>
2020-07-01 09:17:47 -0400

    added sphere for mr1 R

--- receptorInterpolate.py
+++ receptorInterpolate.py
@@ -78,5 +78,44 @@ def receptorInterpolate( slab, out_fn, rec_fn, srv_fn, cls_fn, output_dir, ligan
 
     # 2. Find non-linear transform from autoradiograph GM classified slice to MRI-derived SRV image
     tfm = alignLigandToSRV(df, slab, ligand, srv_fn, cls_fn, output_dir,  tfm_type_2d=tfm_type_2d, clobber=clobber)
+
+    surf_dict = load_mesh_geometry(surf_obj_fn)
+    coords = surf_dict['coords']
+
+    print('Get surface mask and surface values')
+    surface_mask_fn = f'surface_slab_mask_{n_vertices}.txt'
+    surface_val_fn  = f'surface_slab_val_{n_vertices}.txt'
+    surface_mask, surface_val = get_surface_values(mask_src, array_src, coords, surface_mask_fn, surface_val_fn)
+
+
+    print('Min',np.min(surface_val),'Max',np.max(surface_val))
+    #load in sphere mesh
+    print('Load mesh sphere')
+    sphere = load_mesh_geometry(sphere_obj_fn) 
+
+    print(surface_mask.shape)
+    print('Convert to sphereical coordinates')
+    spherical_coords = surface_tools.spherical_np(sphere['coords'])
+    print(spherical_coords.shape)
+
+    spherical_coords_src = spherical_coords[ surface_mask, : ]
+
+    surface_val_src = surface_val[ surface_mask.astype(bool) ]
+    surface_val_tgt = surface_val[ ~surface_mask.astype(bool) ]
+
+    lats_src, lons_src = spherical_coords_src[:,1]-np.pi/2, spherical_coords_src[:,2]
+    lats, lons = spherical_coords[:,1]-np.pi/2, spherical_coords[:,2]
+
+    print('Create Mesh')
+    mesh = stripy.sTriangulation(lons_src,lats_src)
+
+    print('Interpolate')
+    interpolated_data=np.zeros_like(surface_val)
+    interpolated_data[surface_mask]=surface_val_src
+    interp_val, interp_type = mesh.interpolate(lons,lats, zdata=surface_val[surface_mask], order=1)
+    print(interp_val.shape, lats.shape)
+    interpolated_data=interp_val
+    print('Write interpolated values')
+    pd.DataFrame(interpolated_data).to_csv('surface_interpolated_values.txt',index=False)
    
     
