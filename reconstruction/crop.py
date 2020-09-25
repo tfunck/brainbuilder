92e6777bd52355efd0f64ffce6f2f3c3bb6cd01d
tf <thomas.funck@mail.mcgill.ca>
2020-09-25 13:19:20 -0400

    updates to run on server

--- reconstruction/crop.py
+++ reconstruction/crop.py
@@ -29,7 +29,7 @@ def get_base(fn) :
     return fn
 
 
-def crop(src_dir, out_dir, df, out_df_fn,clobber=False):
+def crop(src_dir, out_dir, df, out_df_fn, remote=False,clobber=False):
     pad = 50
     p = int(pad/2)
     
@@ -43,39 +43,41 @@ def crop(src_dir, out_dir, df, out_df_fn,clobber=False):
     files = glob('{}/*'.format(src_dir))
     basenames = [ get_base(fn) for fn in files ]
     n=len(files)
-    for index, (fn, base) in enumerate(zip(files, basenames)) : 
 
-        if 'MR1' not in base : continue
-        crop_fn = '{}/{}.nii.gz'.format(out_dir , base)
+    if not remote :
+        for index, (fn, base) in enumerate(zip(files, basenames)) : 
 
-        if not os.path.exists(crop_fn) or clobber :
-            mask_fn=glob(f'crop/combined_final/mask/{base}*.png')
-            #print(mask_fn)
-            if len(mask_fn) > 0 : 
-                mask_fn = mask_fn[0]
-            else : 
-                print('Skipping', mask_fn)
-                mask_fn = f'crop/combined_final/mask/{base}.png'
-            
-            
-            mask = imageio.imread(mask_fn)
-            img = imageio.imread(fn)
-            img[ mask != np.max(mask) ] = 0
-            img = img.reshape(img.shape[0], img.shape[1])
-            section=df['lin_fn'] == os.path.splitext(os.path.basename(fn))[0]
-           
-            img_pad = np.zeros([img.shape[0]+pad,img.shape[1]+pad])
+            if 'MR1' not in base : continue
+            crop_fn = '{}/{}.nii.gz'.format(out_dir , base)
 
-            img_pad[p:p+img.shape[0],p:p+img.shape[1]] =img
-            img = img_pad
-            try :
-                section_rotation =  df['rotate'].loc[  section ].values[0] 
-                if section_rotation != 0 : img = rotate(img, section_rotation, reshape=False)
-            except IndexError : 
-                pass
+            if not os.path.exists(crop_fn) or clobber :
+                mask_fn=glob(f'crop/combined_final/mask/{base}*.png')
+                #print(mask_fn)
+                if len(mask_fn) > 0 : 
+                    mask_fn = mask_fn[0]
+                else : 
+                    print('Skipping', mask_fn)
+                    mask_fn = f'crop/combined_final/mask/{base}.png'
+                
+                
+                mask = imageio.imread(mask_fn)
+                img = imageio.imread(fn)
+                img[ mask != np.max(mask) ] = 0
+                img = img.reshape(img.shape[0], img.shape[1])
+                section=df['lin_fn'] == os.path.splitext(os.path.basename(fn))[0]
+               
+                img_pad = np.zeros([img.shape[0]+pad,img.shape[1]+pad])
 
-            nib.Nifti1Image(img, [[0.2,0,0,0],[0,0.2,0,0],[0,0,0.2,0],[0,0,0,1]]).to_filename(crop_fn)
-            #imageio.imsave(crop_fn, img)
+                img_pad[p:p+img.shape[0],p:p+img.shape[1]] =img
+                img = img_pad
+                try :
+                    section_rotation =  df['rotate'].loc[  section ].values[0] 
+                    if section_rotation != 0 : img = rotate(img, section_rotation, reshape=False)
+                except IndexError : 
+                    pass
+
+                nib.Nifti1Image(img, [[0.2,0,0,0],[0,0.2,0,0],[0,0,0.2,0],[0,0,0,1]]).to_filename(crop_fn)
+    return df
 
 
 
