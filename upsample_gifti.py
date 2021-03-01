5778dddf9dfe598b1ba8e4eb94f2f140ed69cd52
tf <thomas.funck@mail.mcgill.ca>
2021-03-01 17:07:23 -0500

    added possibility of resampling multiple surfaces according to single file

--- upsample_gifti.py
+++ upsample_gifti.py
@@ -4,6 +4,8 @@ import time
 import sys
 import nibabel as nib
 import matplotlib.pyplot as plt
+import h5py as h5
+import tempfile
 from c_upsample_mesh import upsample, resample
 from re import sub
 from utils.utils import shell
@@ -56,14 +58,12 @@ def find_opposing_polygons(faces):
 
 def sorted_str (l): return ''.join([ str(element) for element in sorted(l) ])
 
-def upsample_edges(coords, faces, faces_dict, i,j,k , resolution, alt_coords) :
+def upsample_edges(coords, faces, coord_normals,  faces_dict, i,j,k , resolution, temp_alt_coords=None) :
     f_i = faces[:,i]
     f_j = faces[:,j]
-    #f_k = faces[:,k]
 
     c_i = coords[f_i] 
-    c_j = coords[f_j] 
-    #c_k = coords[f_k] 
+    c_j = coords[f_j]
 
     ngh = get_ngh(faces)
 
@@ -72,15 +72,23 @@ def upsample_edges(coords, faces, faces_dict, i,j,k , resolution, alt_coords) :
     long_edges = d_ij > resolution
     f_i = faces[:,i]
 
+
+    n_i = coord_normals[f_i]
+    n_j = coord_normals[f_j]
+    new_coord_normals = (n_i[long_edges] + n_j[long_edges])/2.
+    new_coord_normals = np.concatenate([coord_normals, new_coord_normals ])
+
     new_coords = (c_i[long_edges] + c_j[long_edges])/2.
     new_coords = np.concatenate([coords, new_coords])
 
-    new_alt_coords = None
-    if type(alt_coords) == type(np.array([])) :
-        ac_i = alt_coords[f_i] 
-        ac_j = alt_coords[f_j] 
-        new_alt_coords = (ac_i[long_edges] + ac_j[long_edges])/2.
-        new_alt_coords = np.concatenate([alt_coords, new_alt_coords])
+    if temp_alt_coords != None:  
+        for fn in temp_alt_coords.values() :
+            alt_coords = np.load(fn+'.npy')
+            ac_i = alt_coords[f_i] 
+            ac_j = alt_coords[f_j] 
+            new_alt_coords = (ac_i[f_i][long_edges] + ac_j[f_j][long_edges])/3.
+            new_alt_coords = np.concatenate([alt_coords, new_alt_coords])
+            np.save(fn, new_alt_coords)
     
     f_i = faces[:,i]
     n_valid=np.sum(~long_edges)
@@ -153,14 +161,42 @@ def upsample_edges(coords, faces, faces_dict, i,j,k , resolution, alt_coords) :
         ngh[d].append(index) 
         ngh[index]=[a,b,c,d]
 
-
         faces_offset += 2
 
     new_faces = np.unique(new_faces, axis=1)
-    return new_coords, new_faces, faces_dict, new_alt_coords
+  
+    #check that new coordinates are unique
+    unique_shape = np.unique(new_coords,axis=1).shape
+    assert unique_shape == new_coords.shape, 'error: non unique coordinate points created'
+    
+    if type(new_alt_coords) == type(np.array([])) :
+        alt_unique_shape = np.unique(new_alt_coords,axis=1).shape
+        assert alt_unique_shape == new_alt_coords.shape, 'error: non unique coordinate points created'
 
+    return new_coords, new_faces, new_coord_normals, faces_dict
 
-def upsample_gifti(input_fn,upsample_fn,  resolution, alt_input_fn=False,alt_upsample_fn=None, clobber=False):
+def load_alt_coordinates(alt_input_list):
+    n_alt = len(alt_input_list)
+    temp_alt_fn={}
+    #f = h5.File(f'{alt_h5_fn}','w')
+    for i, fn in enumerate(alt_input_list) :
+        alt_coords = nib.load(fn).agg_data('NIFTI_INTENT_POINTSET')
+        temp_fn='{}{}'.format(tempfile.NamedTemporaryFile().name,os.path.basename(sub('.surf.gii','',fn)))
+        np.save(temp_fn, alt_coords)
+        temp_alt_fn[fn] = temp_fn
+    print(temp_alt_fn)
+    return temp_alt_fn
+
+def setup_coordinate_normals(faces,normals,coords):
+    new_normals=np.zeros(faces.shape)
+    coord_normals = np.zeros_like(coords)
+    for (a, b, c), n in zip(faces[0:normals.shape[0]], normals) :
+        coord_normals[a] += n
+        coord_normals[b] += n
+        coord_normals[c] += n
+    return coord_normals
+
+def upsample_gifti(input_fn,upsample_fn,  resolution, alt_input_list=[], clobber=False):
 
     if not os.path.exists(upsample_fn) or clobber :
         #flatten ngh list to pass to c cod 
@@ -168,21 +204,25 @@ def upsample_gifti(input_fn,upsample_fn,  resolution, alt_input_fn=False,alt_ups
         faces = mesh.agg_data('NIFTI_INTENT_TRIANGLE')
         coords= mesh.agg_data('NIFTI_INTENT_POINTSET')
 
+        normals = np.array([ np.cross(coords[b]-coords[a],coords[c]-coords[a]) for a,b,c in faces ])
+
         faces = np.sort(faces, axis=1).astype(np.int32)
         faces_dict={}
         for i, (x,y,z) in enumerate(faces):
-            faces_dict[sorted_str([x,y,z])] = i
-       
-        alt_coords=None
-        if alt_input_fn != None :
-            alt_coords = nib.load(alt_input_fn).agg_data('NIFTI_INTENT_POINTSET')
+            key=sorted_str([x,y,z])
+            faces_dict[key] = i
+      
+        alt_h5_f = None
+        if alt_input_list != [] :
+            temp_alt_coords = load_alt_coordinates(alt_input_list)
         
-        average_normal = np.sum([ np.cross(coords[b]-coords[a],coords[c]-coords[a]) for a,b,c in faces ],axis=0)
+        coord_normals=setup_coordinate_normals(faces,normals,coords)
         
         #calculate edge lengths
         d = [   calc_dist(coords[faces[:,0]], coords[faces[:,1]]),
                 calc_dist(coords[faces[:,1]], coords[faces[:,2]]),
                 calc_dist(coords[faces[:,2]], coords[faces[:,1]])]
+
         max_len = np.round(np.max(d),2)
         avg_len = np.round(np.mean(d),2)
         perc95 = np.round(np.percentile(d, [95])[0],2)
@@ -191,41 +231,48 @@ def upsample_gifti(input_fn,upsample_fn,  resolution, alt_input_fn=False,alt_ups
         start_resolution = max(int(max_len), resolution) 
         for target_resolution in np.arange(start_resolution, resolution-1,-1) :
             while perc95 > target_resolution and max_len > resolution :
-                coords, faces, faces_dict, alt_coords = upsample_edges(coords, faces, faces_dict, 0, 1, 2, target_resolution,alt_coords=alt_coords)
-                coords, faces, faces_dict, alt_coords = upsample_edges(coords, faces, faces_dict, 1, 2, 0, target_resolution,alt_coords=alt_coords)
-                coords, faces, faces_dict, alt_coords = upsample_edges(coords, faces, faces_dict, 2, 1, 0, target_resolution,alt_coords=alt_coords)
+                for i,j,k in ((0,1,2),(1,2,0),(2,1,0)) :
+                    coords, faces, coord_normals, faces_dict = upsample_edges(coords, faces, coord_normals, faces_dict, i, j, k, target_resolution,temp_alt_coords=temp_alt_coords)
                 d = [   calc_dist(coords[faces[:,0]], coords[faces[:,1]]),
                         calc_dist(coords[faces[:,1]], coords[faces[:,2]]),
-                        calc_dist(coords[faces[:,2]], coords[faces[:,1]])]
+                        calc_dist(coords[faces[:,2]], coords[faces[:,0]])]
                 max_len = np.round(np.max(d),2)
                 avg_len = np.round(np.mean(d),2)
                 perc95 = np.round(np.percentile(d, [95])[0],2)
                 print('\tmax edge',max_len,'95th percentile',perc95,'\tavg', avg_len,'\tcoords=', coords.shape[0], '\tfaces=', faces.shape[0] )
 
         print('\tFinal coords=', coords.shape, 'starting faces', faces.shape)
+
+        #fix normals
         for i in range(faces.shape[0]) :
             a,b,c = faces[i]
             test_normal = np.cross(coords[b]-coords[a], coords[c]-coords[a])
+            average_normal=coord_normals[a]+coord_normals[b]+coord_normals[c]
             x=np.dot(average_normal, test_normal)
             if x < 0 : faces[i]=[c,b,a]
 
         print('\tWriting',upsample_fn)
         save_gii( coords, faces, input_fn, upsample_fn )
-        if type(alt_coords) == type(np.array([])) :
-            print('\tWriting',alt_upsample_fn)
-            save_gii( alt_coords, faces, input_fn, alt_upsample_fn )
+        if temp_alt_coords != None :
+            for orig_fn, fn in temp_alt_coords.items() :
+                alt_upsample_fn=sub('.surf.gii','_rsl.surf.gii',orig_fn)
+                print('\tWriting',alt_upsample_fn)
+                save_gii( np.load(fn+'.npy'), faces, input_fn, alt_upsample_fn )
 
-
-
-
-def create_high_res_sphere(input_fn, upsample_fn, sphere_fn, sphere_rsl_fn, resolution, clobber=False ):
+def create_high_res_sphere(input_fn, upsample_fn, resolution, niters=200, clobber=False ):
+    sphere_fn = sub('.surf.gii','_sphere.surf.gii',args.input_fn)
+    sphere_rsl_fn = sub('.surf.gii','_sphere_rsl.surf.gii',args.input_fn)
+    alt_input_list=[sphere_fn]
+    alt_output_list=[sphere_rsl_fn]
     if not os.path.exists(sphere_fn) or  clobber :
         print('\tInflate to sphere')
-        shell('~/freesurfer/bin/mris_inflate -n 200  {} {}'.format(input_fn, sphere_fn))
+        #shell('~/freesurfer/bin/mris_inflate -n {}  {} {}'.format( niters, input_fn, sphere_fn))
+        shell('mris_inflate -n {}  {} {}'.format( niters, input_fn, sphere_fn))
         #shell('mris_inflate -n 200  {} {}'.format(input_fn, sphere_fn))
-    resolution=1
+    #resolution=1 #DELETE ME
+    clobber=True
     if not os.path.exists(upsample_fn) or clobber :
-        upsample_gifti( input_fn, upsample_fn, float(resolution), alt_input_fn=sphere_fn,  alt_upsample_fn=sphere_rsl_fn, clobber=clobber)
+        upsample_gifti( input_fn, upsample_fn, float(resolution), alt_input_list=alt_input_list,  clobber=clobber)
 
 
 if __name__ == "__main__" :
@@ -233,13 +280,12 @@ if __name__ == "__main__" :
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('-i', dest='input_fn',  default=None, help='input fn')
     parser.add_argument('-r', dest='resolution',  default=None, help='resolution')
-    parser.add_argument('-a', dest='alt_input_fn',  default=None, help='optional fn')
+    parser.add_argument('-a', dest='alt_input_list', nargs='+', default=[], help='optional fn')
     parser.add_argument('-c', action='store_true', default=False, dest='clobber', help='optional fn')
+    parser.add_argument('-n',  default=300, dest='niters', help='niters')
     args = parser.parse_args()
 
 
     upsample_fn = sub('.surf.gii','_rsl.surf.gii',args.input_fn)
-    sphere_fn = sub('.surf.gii','_sphere.surf.gii',args.input_fn)
-    sphere_rsl_fn = sub('.surf.gii','_sphere_rsl.surf.gii',args.input_fn)
     print('Upsample fn', upsample_fn)
-    create_high_res_sphere(args.input_fn, upsample_fn, sphere_fn, sphere_rsl_fn, args.resolution,clobber=args.clobber)
+    create_high_res_sphere(args.input_fn, upsample_fn,  args.resolution,niters=args.niters,clobber=args.clobber)
