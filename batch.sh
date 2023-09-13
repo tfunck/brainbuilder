451826cc984bba544da9582905a5941508c99bb1
tfunck <tffunck@gmail.com>
2023-09-13 16:23:47 -0400

    tried to clean up dir

--- batch.sh
+++
@@ -1,6 +0,0 @@
-export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=14
-
-for slab in 1 2 3 4 5 6 ; do
-    sh launch_reconstruction.sh -s $slab -b MR1 -m R
-done
-exit 0
--- build_singularity.sh
+++
@@ -1,2 +0,0 @@
-#sudo singularity build base.simg build/base/Singularity 
-sudo singularity build receptor-v.1.2.simg Singularity 
