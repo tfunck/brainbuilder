7c89dac2f0798da8f2c39427b4583534c61246df
tfunck <tffunck@gmail.com>
2023-09-15 16:05:10 -0400

    added setup.py back in

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='brainbuilder',
+      version='0.0',
+      description='Reconstruction of 3D brains from 2D images',
+      author='Thomas Funck',
+      author_email='thomas.funck@childmind.org',
+      url='https://github.com/tfunck/brainbuilder',
+      packages=['brainbuilder']
+     )
