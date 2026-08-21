15e04887a9578c47a75bf49e5a23bb1eaaa4484a
tfunck <tffunck@gmail.com>
2026-08-21 13:32:26 -0400

    update

--- docker/build.sh
+++ docker/build.sh
@@ -1,1 +1,1 @@
- docker build --no-cache -t tffunck/brainbuilder:v.${1} -t tffunck/brainbuilder:dev .
+DOCKER_BUILDKIT=1 docker build --no-cache -t tffunck/brainbuilder:v.${1} -t tffunck/brainbuilder:dev .
